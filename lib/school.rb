f3ab283e1073464d32dc8b79517f9f7134f10548
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-15 19:22:09 -0400

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require "pry"
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+    end
+
+end
+
+school = School.new("Bayside High School")
+
+#binding.pry
